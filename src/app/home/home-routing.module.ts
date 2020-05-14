9aa1ef26f3f27f7db9c7edfd2c390278c3ba5473
ISSAM <issamelgharabi@gmail.com>
2020-05-14 22:46:28 +0000

    creation de home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,13 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPage } from '../about/about.page';
+import { ContactPage } from '../contact/contact.page';
+import { NewsPage } from '../news/news.page';
 
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomePage
-  }
+    path: 'tabs',
+    component: HomePage,
+    children: [
+      {
+        path: 'news',
+        outlet: 'news',
+        component: NewsPage
+      },
+      {
+        path: 'about',
+        outlet: 'about',
+        component: AboutPage
+      },
+      {
+        path: 'contact',
+        outlet: 'contact',
+        component: ContactPage
+      }
+    ]
+  },
+
+{
+  path: '',
+  redirectTo: '/home/(news:news)',
+  pathMatch: 'full'
+}
 ];
 
 @NgModule({
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -5,32 +5,7 @@ import { LoginPage } from './login.page';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'registre',
-    loadChildren: () => import('./registre/registre.module').then( m => m.RegistrePageModule)
-  },
-  {
-    path: 'news',
-    loadChildren: () => import('./news/news.module').then( m => m.NewsPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+    component: LoginPage
   }
 
 ];
